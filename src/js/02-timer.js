3733f0f746c97d2b9a396f1969a3a454b5a6d1c1
dkusnieruk <dariusz.kusnieruk@wp.pl>
2022-10-08 18:45:18 +0200

    update task 2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -10,8 +10,41 @@ getInput= document.querySelector(`input`);
 getData = document.getElementById(`datetime-picker`);
 
 getButton = document.querySelector(`button`);
+console.log(getButton);
 
 getButton.disabled = true;
+
+getSpan = document.getElementsByClassName(`value`);
+
+getDay = getSpan[0];
+getHour = getSpan[1];
+getMinute=getSpan[2];
+getSecond= getSpan[3];
+
+
+
+
+
+function convertMs(ms) {
+    // Number of milliseconds per unit of time
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+  
+    // Remaining days
+    const days = Math.floor(ms / day);
+    // Remaining hours
+    const hours = Math.floor((ms % day) / hour);
+    // Remaining minutes
+    const minutes = Math.floor(((ms % day) % hour) / minute);
+    // Remaining seconds
+    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  
+    return { days, hours, minutes, seconds };
+    
+  }
+  
 
 const options = {
     enableTime: true,
@@ -35,36 +68,46 @@ const options = {
           );
           getButton.disabled= true;
         } else {
-            const ms = selectedDates[0] - new Date();
+            let ms = selectedDates[0] - new Date();
             console.log(ms);
             getButton.disabled= false;
-        };
-      }
+            let getVariable =convertMs(ms);
+            getDay.innerHTML = getVariable.days;
+            getHour.innerHTML= getVariable.hours;
+            getMinute.innerHTML=getVariable.minutes;
+            getSecond.innerHTML=getVariable.seconds;
+            let timerId=null;
+
+getButton.addEventListener(`click`, (event)=>{
+    timerId = setInterval(() => {
+        const minus = 1000;
+        ms -= minus;
+        getVariable =convertMs(ms);
+
+
+        // console.log(ms);
+        // console.log(getVariable);
+            
+            getDay.innerHTML = getVariable.days;
+            getHour.innerHTML= getVariable.hours;
+            getMinute.innerHTML=getVariable.minutes;
+            getSecond.innerHTML=getVariable.seconds;
+            if( getVariable.seconds===0){
+                clearInterval(timerId);
+            };
+      }, 1000);
+      console.log(ms);
+
+})
+            
+
+            };
+        }
 
     };
 
-    function convertMs(ms) {
-        // Number of milliseconds per unit of time
-        const second = 1000;
-        const minute = second * 60;
-        const hour = minute * 60;
-        const day = hour * 24;
-      
-        // Remaining days
-        const days = Math.floor(ms / day);
-        // Remaining hours
-        const hours = Math.floor((ms % day) / hour);
-        // Remaining minutes
-        const minutes = Math.floor(((ms % day) % hour) / minute);
-        // Remaining seconds
-        const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-      
-        return { days, hours, minutes, seconds };
-      }
-      convertMs();
-      console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-      console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-      console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+    
+    flatpickr(getData, options, );
 
     
-flatpickr(getData, options, );
+
