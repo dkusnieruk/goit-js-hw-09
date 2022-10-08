30ac76cda5105251f5888926f665323527ed6f3b
dkusnieruk <dariusz.kusnieruk@wp.pl>
2022-10-08 06:36:21 +0200

    update task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
-//JS mend 
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+const getButton = document.getElementsByTagName(`button`);
+console.log(getButton);
+
+getButton[0].addEventListener(`click`, (event)=>{
+timeriD =setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+}, 1000);
+event.target.disabled = true;
+getButton[1].disabled = false;
+} );
+
+getButton[1].addEventListener(`click`, (event)=>{
+    clearInterval(timeriD);
+    event.target.disabled= true;
+    getButton[0].disabled = false;
+
+})
