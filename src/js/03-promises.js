6d589193083883d7b221f3c93a099d340d0571e6
dkusnieruk <dariusz.kusnieruk@wp.pl>
2022-10-12 17:51:29 +0200

    update task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,38 +1,38 @@
 import Notiflix from 'notiflix';
 
 
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    console.log('fullfill');
-    Notiflix.Notify.warning(
-      `✅ Fulfilled promise ${position} in ${delay}ms`,
-      {
-      width:`300px`,
-      useFontAwesome: true,
-      warning: {
-          background: `green`,
-          textColor:`white`,
-      }
-      },
-    );
-    // Fulfill
-  } else {
-    console.log('reject');
-    Notiflix.Notify.warning(
-      `❌ Rejected promise ${position} in ${delay}ms`,
-      {
-      width:`300px`,
-      useFontAwesome: true,
-      warning: {
-          background: `red`,
-          textColor:`white`,
-      }
-      },
-    );
-    // Reject
-  }
-}
+// const promise =function createPromise(position, delay) {
+//   const shouldResolve = Math.random() > 0.3;
+//   if (shouldResolve) {
+//     console.log('fullfill');
+//     Notiflix.Notify.warning(
+//       `✅ Fulfilled promise ${position} in ${delay}ms`,
+//       {
+//       width:`300px`,
+//       useFontAwesome: true,
+//       warning: {
+//           background: `green`,
+//           textColor:`white`,
+//       }
+//       },
+//     );
+//     // Fulfill
+//   } else {
+//     console.log('reject');
+//     Notiflix.Notify.warning(
+//       `❌ Rejected promise ${position} in ${delay}ms`,
+//       {
+//       width:`300px`,
+//       useFontAwesome: true,
+//       warning: {
+//           background: `red`,
+//           textColor:`white`,
+//       }
+//       },
+//     );
+//     // Reject
+//   }
+// }
 
 
 
@@ -49,16 +49,53 @@ getButton.addEventListener(`click`,(event)=>{
   const position = getAmount[0].value;
   const delay=getStep[0].value;
   const step=getDelay[0].value;
+  
 
 let counter = 0;
 
+
+
 let timer = setInterval(function() {
   
-createPromise(position,delay);
-
     counter += 1;
+    const getOutput = +step + (+delay*(counter-1));
+    console.log(getOutput);
+const obietnica = new Promise ((resolve, reject)=>{
   
+    const shouldResolve = Math.random() > 0.3;
+    if (shouldResolve){
+      resolve(
+        Notiflix.Notify.warning(
+          `✅ Fulfilled promise ${counter} in ${getOutput}ms`,
+          {
+          width:`300px`,
+          useFontAwesome: true,
+          warning: {
+              background: `green`,
+              textColor:`white`,
+          }
+          },
+        )
+      )
+    } else {
+      reject(
+        Notiflix.Notify.warning(
+          `❌ Rejected promise ${counter} in ${getOutput}ms`,
+          {
+          width:`300px`,
+          useFontAwesome: true,
+          warning: {
+              background: `red`,
+              textColor:`white`,
+          }
+          },
+        )
+        
+      )
+    }
 
+  
+})
 
     if (counter >= position) {
         clearInterval(timer);
